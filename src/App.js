5c8a761cace2833527e458933fe3fb56f2035b52
miramonster <95644636+miramonster@users.noreply.github.com>
2022-03-18 01:12:15 -0500

    pin fixed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ import {
   ComboboxOption,
 } from "@reach/combobox";
 
-
 import "@reach/combobox/styles.css";
 
 const libraries = ["places"];
@@ -41,33 +40,98 @@ const options = {
 };
 export default function App() {
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyChx65TaOoV6FVspAn917zZm2siJHJsuEg',
+    googleMapsApiKey: "AIzaSyChx65TaOoV6FVspAn917zZm2siJHJsuEg",
     libraries,
   });
+
+  const [userGeo, setUserGeo] = React.useState(null);
+
   const [markers, setMarkers] = React.useState([]);
 
-  const onMapClick = React.useCallback((event) => {
-    setMarkers((current) => [
-      ...current,
-      {
-        lat: event.latLng.lat(),
-        lng: event.latLng.lng(),
-        time: new Date(),
-      },
-    ]);
-  }, []);
+  // const onMapClick = React.useCallback((event) => {
+  //   setMarkers((current) => [
+  //     ...current,
+  //     {
+  //       lat: event.latLng.lat(),
+  //       lng: event.latLng.lng(),
+  //       time: new Date(),
+  //     },
+  //   ]);
+  // }, []);
 
   const mapRef = React.useRef();
+  let placesService;
+
   const onMapLoad = React.useCallback((map) => {
     mapRef.current = map;
   }, []);
 
   const panTo = React.useCallback(({ lat, lng }) => {
     mapRef.current.panTo({ lat, lng });
-    mapRef.current.setZoom(6);
+    mapRef.current.setZoom(12);
   }, []);
 
   const [selected, setSelected] = React.useState(null);
+
+  const radialSearchTrigger = React.useCallback(
+    (userPosition, placesService) => {
+      const searchCircle = new window.google.maps.Circle({
+        center: userPosition,
+        radius: 1609.344 * 25, // rough convertion from meters to miles
+      });
+      // console.log(placesService)
+      placesService.textSearch(
+        {
+          query: "Pizza",
+          // openNow: true,
+          location: userPosition,
+          radius: 1609.344 * 50,
+          type: ["resturaunt"],
+        },
+        (results, status) => {
+          console.log(status);
+          if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+            setMarkers(results);
+          }
+        }
+      );
+    },
+    [mapRef.current]
+  );
+
+  React.useEffect(() => {
+    console.log("userGeo: ", userGeo);
+    if (mapRef.current && userGeo) {
+      placesService = new window.google.maps.places.PlacesService(
+        mapRef.current
+      );
+      radialSearchTrigger(userGeo, placesService);
+    }
+  }, [mapRef.current, userGeo]);
+
+  const markerMaker = (marker) => {
+    const {geometry, name, place_id} = marker
+    return (
+      <Marker
+        key={place_id}
+        position={{
+          lat: geometry.location.lat(),
+          lng: geometry.location.lng(),
+        }}
+        title={name}
+        onClick={() => {
+          setSelected({
+            lat: geometry.location.lat(),
+            lng: geometry.location.lng(),
+          });
+        }}
+        icon={{
+          url: "./topPizza.png",
+          scaledSize: new window.google.maps.Size(30, 30),
+        }}
+      />
+    );
+  };
 
   if (loadError) return "Error loading maps";
 
@@ -77,36 +141,36 @@ export default function App() {
     <div>
       <h1>
         Pizza Hunter
-        <img src={"./topPizza.png"} width="auto" height="40" />
+        <img src={"./topPizza.png"} width="auto" height="80" />
       </h1>
 
-      <Locate panTo={panTo} />
-      <Search panTo={panTo} />
-      
+      <Locate
+        panTo={panTo}
+        setUserGeo={setUserGeo}
+        radialSearchTrigger={radialSearchTrigger}
+      />
+      <Search panTo={panTo} setUserGeo={setUserGeo} />
 
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={8}
         center={center}
         options={options}
-        onClick={onMapClick}
+        // onClick={onMapClick}
         onLoad={onMapLoad}
       >
-         {markers.map((marker) => (
+        {userGeo?.lat && userGeo?.lng && (
           <Marker
-            key={`${marker.lat}-${marker.lng}`}
-            position={{ lat: marker.lat, lng: marker.lng }}
-            onClick={() => {
-              setSelected(marker);
-            }}
+            key="userPosition"
+            position={userGeo}
             icon={{
-              url:"./topPizza.png",
-              origin: new window.google.maps.Point(0, 0),
-              anchor: new window.google.maps.Point(15, 15),
-              scaledSize: new window.google.maps.Size(30, 30),
-            }}           
+              url: "./meeple.png",
+              scaledSize: new window.google.maps.Size(60, 60),
+            }}
           />
-        ))}
+        )}
+               
+        {markers.map(markerMaker)}
         {selected ? (
           <InfoWindow
             position={{ lat: selected.lat, lng: selected.lng }}
@@ -117,7 +181,7 @@ export default function App() {
             <div>
               <h2>
                 <span role="img" aria-label="pizza">
-                🍕
+                  🍕
                 </span>{" "}
                 place holder
               </h2>
@@ -129,28 +193,30 @@ export default function App() {
     </div>
   );
 }
-function Locate({ panTo }) {
+
+function Locate({ panTo, setUserGeo, radialSearchTrigger }) {
   return (
-    <button 
-    className="locate"
-    onClick={() => {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          panTo({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          });
-        },
-        () => null
-      );
-    }}
-  >
+    <button
+      className="locate"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            const userPosition = { lat: latitude, lng: longitude };
+            panTo(userPosition);
+            setUserGeo(userPosition);
+            //  radialSearchTrigger(userPosition);
+          },
+          () => null //TODO: Error Handle
+        );
+      }}
+    >
       <img src={"./compassRose.png"} alt="compass locate me" />
     </button>
   );
 }
 
-function Search({ panTo }) {
+function Search({ panTo, setUserGeo }) {
   const {
     ready,
     value,
@@ -169,12 +235,13 @@ function Search({ panTo }) {
       <Combobox
         onSelect={async (address) => {
           setValue(address, false);
-          clearSuggestion();
+          //clearSuggestion();
 
           try {
             const results = await getGeocode({ address });
             const { lat, lng } = await getLatLng(results[0]);
             panTo({ lat, lng });
+            setUserGeo({ lat, lng });
           } catch (error) {
             console.log("error");
           }
@@ -191,7 +258,6 @@ function Search({ panTo }) {
           placeholder="Enter an address"
         />
         <ComboboxPopover>
-          {console.log(data)};
           <ComboboxList>
             {status === "OK" &&
               data.map(({ place_id, description }) => (
